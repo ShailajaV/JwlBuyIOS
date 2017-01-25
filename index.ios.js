c18674502a6aa3d5d80218a206f9f1390ca9ab68
ShailajaV <shailaja.v8@gmail.com>
2017-01-25 08:27:21 -0600

    second commit-shailu

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+/**
+ * Jewellery Buyer React Native App
+ */
+
+import {
+  AppRegistry
+} from 'react-native';
+import App from './src/App';
+
+AppRegistry.registerComponent('JwlBuyIOS', () => App);
